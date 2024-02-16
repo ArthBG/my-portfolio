e76e947dc24407746b677c6aeff3073b6ab590d3
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-02-16 19:55:14 -0300

    Atualizações no tailwind.config.js, package.json e App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { useState } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
 
 export default function App() {
+  
   const projects = [
     {
       name: "OctoPlay",
@@ -19,30 +21,39 @@ export default function App() {
       link: "royaleoc.netlify.app"
     }
   ]
+
+  const _renderItem = ({ item, index }) => {
+    return (
+      <View>
+        <Image source={item.image} style="w-full h-48 object-cover" />
+        <Text className="font-bold text-lg">{item.name}</Text>
+        <Text>{item.description}</Text>
+        <Text>{item.tecnologys}</Text>
+        <Text>{item.link}</Text>
+      </View>
+    );
+  };
   return (
-    <View>
-      <View className="bg-blue-950 min-h-screen">
+    <View  className="bg-slate-200"> 
+      <View>
       <Image className="rounded-full w-80 h-80 mx-auto" source={require('./assets/images/icon.jpeg')} />
 
       <Text className="font-extrabold text-2xl mx-auto pt-7">Arthur Borges</Text>
       <Text className="text-lg mx-auto pt-5" ></Text>
       <Text className="text-lg mx-auto pt-2" >Estudante de Desenvolvimento de Sistemas no SENAI Valinhos{'\n'}Cursando o terceiro ano do Ensino Médio</Text>
+      <ScrollView>
       <Text className="font-semibold text-2xl mx-auto pt-5">Sobre:</Text>
       <Text className="text-base mx-auto pt-2" >Desenvolvedor Full Stack com conhecimentos em front-end (HTML, CSS, JavaScript, React, React Native). Especializado na criação de interfaces intuitivas e eficiência na construção de softwares. Comprometido com as melhores práticas e atualizações constantes, estou pronto para contribuir significativamente em projetos desafiadores.</Text>
-      <ScrollView className="h-fit">
-      <Text className="font-semibold text-2xl mx-auto pt-7" >Projetos:</Text>
-        {projects.map((project, key) => (
-          <View key={key}>
-            <View className="h-fit mx-auto mt-2">
-            <Image source={project.image} className="h-64 w-85" />
-            <Text className="text-lg font-bold pt-2">{project.name}</Text>
-            <Text className="text-base">{project.description}</Text>
-            <Text className="text-base font-medium pt-2">Tecnologias Utilizadas: </Text>
-            <Text className="text-base font-medium">{project.tecnologys}</Text>
-            <Text className="text-base">{project.link}</Text>
-            </View>
-          </View>
-        ))}
+        <View className="pt-5">
+          <Text className="font-bold text-xl text-center pt-2">Projetos:</Text>
+          <Carousel
+            ref={(c) => { this._carousel = c; }}
+            data={projects}
+            renderItem={_renderItem}
+            sliderWidth={300}
+            itemWidth={300}
+          />
+        </View>
       </ScrollView>
       <StatusBar style="auto" />
       </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    textColor: {
-      'white': '#ffffff',
-    },
   },
   variants: {},
   plugins: [],
