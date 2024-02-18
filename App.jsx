d02d8f5365b25ea0e94e5018e53e20647f72c221
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-02-18 12:02:45 -0300

    Refactor project cards and contact information

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 
@@ -13,6 +13,7 @@ export default function App() {
   
   const projects = [
     {
+      id: 1,
       name: "OctoPlay",
       description: "Projeto solicitado pelo curso do SENAI Valinhos, com o objetivo de criar uma biblioteca de jogos digitais.",
       tecnologys: "HTML, CSS, JavaScript, React, Node.js, Vercel.",
@@ -20,12 +21,30 @@ export default function App() {
       link: "octoplay.vercel.app"
     },
     {
+      id: 2,
       name: "RoyaleOc",
       description: "Projeto de com o objetivo de criar um site completo sobre o jogo Clash Royale.",
       tecnologys: "HTML, CSS, JavaScript, React, Node.js, Netlify.",
       image: require('./assets/images/royaleoc.png'),
       link: "royaleoc.netlify.app"
-    }
+    },
+    {
+      id: 3,
+      name: "OctoPlay",
+      description: "Projeto solicitado pelo curso do SENAI Valinhos, com o objetivo de criar uma biblioteca de jogos digitais.",
+      tecnologys: "HTML, CSS, JavaScript, React, Node.js, Vercel.",
+      image: require('./assets/images/octoplay.png'),
+      link: "octoplay.vercel.app"
+    },
+    {
+      id: 4,
+      name: "RoyaleOc",
+      description: "Projeto de com o objetivo de criar um site completo sobre o jogo Clash Royale.",
+      tecnologys: "HTML, CSS, JavaScript, React, Node.js, Netlify.",
+      image: require('./assets/images/royaleoc.png'),
+      link: "royaleoc.netlify.app"
+    },
+
   ]
 
   return (
@@ -35,8 +54,8 @@ export default function App() {
 
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-extrabold text-2xl mx-auto pt-7">Arthur Borges</Text>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-lg mx-auto pt-2" >Estudante de Desenvolvimento de Sistemas no SENAI Valinhos{'\n'}Cursando o terceiro ano do Ensino Médio</Text>
-      <View className="flex justify-center mx-auto">
-        <TouchableOpacity onPress={darkModeToggle} className="bg-gray-300 w-20 h-10 rounded-full mt-5">
+      <View className="flex justify-center m-auto">
+        <TouchableOpacity onPress={darkModeToggle} className="w-20 h-10 rounded-full mt-5">
           <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-center pt-2">Dark Mode</Text>
         </TouchableOpacity>
       </View>
@@ -45,15 +64,16 @@ export default function App() {
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto pt-2" >Desenvolvedor Full Stack com conhecimentos em front-end (HTML, CSS, JavaScript, React, React Native). Especializado na criação de interfaces intuitivas e eficiência na construção de softwares. Comprometido com as melhores práticas e atualizações constantes, estou pronto para contribuir significativamente em projetos desafiadores.</Text>
         <View className="pt-5">
           <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold text-xl text-center pt-2">Projetos:</Text>
-       <ScrollView horizontal>
-          <View className="mx-auto">
+       {/* <ScrollView horizontal> */}
+          <View className="content-center items-center justify-center text-center">
             {
-              projects.map((project, index) => {
+              projects.map((project) => {
                 return (
-                  <View key={index} className="w-80 h-80">
-                    <Image source={project.image} className="w-80 h-64" />
-                    <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold text-lg text-center">{project.name}</Text>
-                    <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-center">{project.description}</Text>
+                  <View key={project.id}>
+                    <Image source={project.image} className="w-96 h-64 rounded-lg mt-8 mx-5" />
+                    <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold text-xl text-center">{project.name}</Text>
+                    <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-justify mx-5">{project.description}</Text>
+                    <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-semibold text-base text-center m-3">Tecnologias Utilizadas:</Text>
                     <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-center">{project.tecnologys}</Text>
                     <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-center">{project.link}</Text>
                   </View>
@@ -62,28 +82,28 @@ export default function App() {
             }
             </View>
             
-              </ScrollView> 
+              {/* </ScrollView>  */}
         </View>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-semibold text-2xl mx-auto pt-5">Contato:</Text>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto pt-2">
         <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">E-mail:</Text>
         <Image className="w-10 h-10" source={require('./assets/images/gmail.png')} />
         <Text>
-          'janbgsart@gmail.com'
+          janbgsart@gmail.com
         </Text>
       </Text>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto pt-2">
         <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">Telefone:</Text>
         <Image className="w-10 h-10" source={require('./assets/images/whatsapp.png')} />
         <Text>
-          '(19) 9 9713-7980'
+          (19) 9 9713-7980
         </Text>
       </Text>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto pt-2">
         <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">LinkedIn:</Text>
         <Image className="w-10 h-10" source={require('./assets/images/linkedin.png')} />
         <Text>
-          'https://www.linkedin.com/in/arthur-borges-b3877a2b1/'
+          https://www.linkedin.com/in/arthur-borges-b3877a2b1/
         </Text>
       </Text>
       </ScrollView>
