a1d926cdb35420541b2d4acc71bdd4e5fa8f691d
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2024-02-18 13:47:37 -0300

    Refactor contact information styling

--- App.jsx
+++ App.jsx
@@ -86,23 +86,23 @@ export default function App() {
         </View>
       <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-semibold text-2xl mx-auto pt-10">Contato:</Text>
       <View>
-      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto m-2">
-        <Image className="w-16 h-20" source={require('./assets/images/gmail.png')} />
-        <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">E-mail: </Text>
+      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base m-2 p-8">
+        <Image className="w-16 h-16" source={require('./assets/images/gmail.png')} />
+        <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">  E-mail: </Text>
         <Text>
           janbgsart@gmail.com
         </Text>
       </Text>
-      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto m-2">
+      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base m-2 p-8">
         <Image className="w-16 h-20" source={require('./assets/images/whatsapp.png')} />
         <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">Telefone: </Text>
         <Text>
           (19) 9 9713-7980
         </Text>
       </Text>
-      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base mx-auto m-2">
+      <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="text-base m-2 p-8">
         <Image className="w-16 h-20" source={require('./assets/images/linkedin.png')} />
-        <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">LinkedIn</Text>
+        <Text style={darkMode? {color: 'white'} : {color: 'black'}} className="font-bold">  LinkedIn</Text>
         {/* <Text>
           https://www.linkedin.com/in/arthur-borges-b3877a2b1/
         </Text> */}
